7f8f5350e0c53b2030f641db1e711c2140ec507e
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-03-22 12:57:26 -0400

    Initial Commit

---
+++ goalpost-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Austin Rightnowar on 3/22/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
